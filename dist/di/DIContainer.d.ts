f05cb0ea8b6be9a630463f6e906ace46889663d0
Davuth Oung <70792355+OungDavuth04@users.noreply.github.com>
2025-01-23 15:20:09 +0700

    update build

---
+++ dist/di/DIContainer.d.ts
@@ -0,0 +1,15 @@
+declare class DIContainer {
+    private static instance;
+    private services;
+    private instances;
+    private constructor();
+    static getInstance(): DIContainer;
+    register<T>(target: {
+        new (...args: any[]): T;
+    }): void;
+    resolve<T>(target: {
+        new (...args: any[]): T;
+    }): T;
+}
+export declare const container: DIContainer;
+export {};
