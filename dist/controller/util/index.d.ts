1093431328affd02ef5307ab8e7d7f3cdccda514
HitTer <davuthlove96@gmail.com>
2025-03-23 13:48:10 +0700

    build prod socket feature

--- dist/controller/util/index.d.ts
+++ dist/controller/util/index.d.ts
@@ -1,6 +1,5 @@
 import { CoreMiddleware, ErrorInterceptor, Interceptor } from "../interface";
 import { NextFunction, Request, Response } from "express";
-import { Socket } from "socket.io";
 type HttpMethod = 'get' | 'post' | 'put' | 'delete' | 'patch' | 'event';
 /**
  * Loads all exported classes from the given directory.
@@ -12,11 +11,4 @@ export declare function isInterceptor(obj: Interceptor): obj is Interceptor;
 export declare function isInterceptorError(obj: ErrorInterceptor): obj is ErrorInterceptor;
 export declare function isMiddleware(obj: CoreMiddleware): obj is CoreMiddleware;
 export declare function executeRoute(this: any, request: Request, response: Response, next: NextFunction): Promise<void>;
-export declare class SocketObserver {
-    private events;
-    constructor();
-    subscribe(event: string, callback: (data: any, socket: Socket) => void): void;
-    unsubscribe(event: string, callback: (data: any, socket: Socket) => void): void;
-    notify(event: string, data: any, socket: Socket): void;
-}
 export {};
