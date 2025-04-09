15bdadca8443c4a20ba81d321f7edbf0d1395da9
Davuth Oung <70792355+OungDavuth04@users.noreply.github.com>
2025-04-09 15:01:20 +0700

    update socket decorator version

---
+++ dist/controller/decorator/SocketData.d.ts
@@ -0,0 +1,2 @@
+import { ClassTransformOptions } from "class-transformer/types/interfaces";
+export declare function SocketData(options?: ClassTransformOptions): (target: any, propertyKey: string | symbol, parameterIndex: number) => void;
