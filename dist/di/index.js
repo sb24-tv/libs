e0731c5cab7c1cb07691e2c91ea58bafce6810b8
Davuth Oung <70792355+OungDavuth04@users.noreply.github.com>
2025-04-17 13:16:40 +0700

    update feature

--- dist/di/index.js
+++ dist/di/index.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Inject = exports.Injectable = exports.container = void 0;
-var DIContainer_1 = require("./DIContainer");
-Object.defineProperty(exports, "container", { enumerable: true, get: function () { return DIContainer_1.container; } });
+var di_container_1 = require("./di-container");
+Object.defineProperty(exports, "container", { enumerable: true, get: function () { return di_container_1.container; } });
 var Injectable_1 = require("./decorators/Injectable");
 Object.defineProperty(exports, "Injectable", { enumerable: true, get: function () { return Injectable_1.Injectable; } });
 var Inject_1 = require("./decorators/Inject");
